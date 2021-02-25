77ae00c629920cb02e1e4810431f7f0b3fde638d
GirivelR <66110878+GirivelR@users.noreply.github.com>
2021-02-25 13:48:44 +0530

    Delete updated directory

--- updated/app/components/lists/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ListItem } from "./ListItem";
-export { default as ListItemDeleteAction } from "./ListItemDeleteAction";
-export { default as ListItemSeparator } from "./ListItemSeparator";
-export { default as ListItemCart } from "./ListItemCart";
